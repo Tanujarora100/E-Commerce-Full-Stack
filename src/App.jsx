266d9048be1b0fa276c0ad8bb2252993b8c3b240
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-15 09:42:03 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+// Import necessary dependencies
+import React from 'react';
+import './App.css';
+
+// TODO: Implement features
+// - [JIRA-123] Dark Mode
+// - [JIRA-124] Sign Up and Log In Functionality
+// - [JIRA-125] Create a Sign Up and Log In Functionality Using Formik
+// - [JIRA-126] Create an express Server With Proper Authentication and MiddleWares
+// - [JIRA-127] Connect the app to Mongo DB
+// - [JIRA-128] Save the user and render the state here in the card with the data in cart functionality
+
+function App() {
+  // TODO: Add state and logic for features
+
+  return (
+    <div>
+      {/* TODO: Implement your UI components here */}
+      <h1>Your App</h1>
+
+      {/* Feature 1: Dark Mode */}
+      {/* TODO: Implement Dark Mode toggle button or functionality */}
+
+      {/* Feature 2: Sign Up and Log In Functionality */}
+      {/* TODO: Implement Sign Up and Log In components or modals */}
+
+      {/* Feature 3: Create a Sign Up and Log In Functionality Using Formik */}
+      {/* TODO: Use Formik for Sign Up and Log In forms */}
+
+      {/* Feature 4: Create an Express Server With Proper Authentication and MiddleWares */}
+      {/* TODO: Set up an Express server with authentication and middleware */}
+
+      {/* Feature 5: Connect the app to MongoDB */}
+      {/* TODO: Set up MongoDB connection and implement necessary queries */}
+
+      {/* Feature 6: Save the user and render the state here in the card with the data in cart functionality */}
+      {/* TODO: Implement user data saving and render the state in a card or component */}
+    </div>
+  );
+}
+
+export default App;
