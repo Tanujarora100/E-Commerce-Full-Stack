1d726863f456b5c7b2fdfa099e85054109cb54a0
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-16 00:08:37 +0530

    basic Routing added :

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,12 @@
 // Import necessary dependencies
 import React from 'react';
 import './App.css';
-
+import { Provider } from "react-redux"
+import store from './redux/store';
+import NavBar from './components/NavBar';
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 // TODO: Implement features
 // - [JIRA-123] Dark Mode
 // - [JIRA-124] Sign Up and Log In Functionality
@@ -18,40 +23,18 @@ function App() {
   // TODO: Add state and logic for features
 
   return (
-    <div>
-      {/* TODO: Implement your UI components here */}
-      <h1>Your App</h1>
+    <Provider store={store}>
+      <div>
+        <div>
+          <NavBar></NavBar>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Cart></Cart>}></Route>
+        </Routes>
 
-      {/* Feature 1: Dark Mode */}
-      {/* TODO: Implement Dark Mode toggle button or functionality */}
-
-      {/* Feature 2: Sign Up and Log In Functionality */}
-      {/* TODO: Implement Sign Up and Log In components or modals */}
-
-      {/* Feature 3: Create a Sign Up and Log In Functionality Using Formik */}
-      {/* TODO: Use Formik for Sign Up and Log In forms */}
-
-      {/* Feature 4: Create an Express Server With Proper Authentication and MiddleWares */}
-      {/* TODO: Set up an Express server with authentication and middleware */}
-
-      {/* Feature 5: Connect the app to MongoDB */}
-      {/* TODO: Set up MongoDB connection and implement necessary queries */}
-
-      {/* Feature 6: Save the user and render the state here in the card with the data in cart functionality */}
-      {/* TODO: Implement user data saving and render the state in a card or component */}
-
-      {/* Feature 7: Add FAQ Section */}
-      {/* TODO: Implement a section with frequently asked questions and answers */}
-
-      {/* Feature 8: Implement Feedback Mechanism */}
-      {/* TODO: Set up a mechanism for users to provide feedback on the application */}
-
-      {/* Feature 9: Implement Product Filtering by Category */}
-      {/* TODO: Add functionality to filter products based on categories */}
-
-      {/* Feature 10: Implement Pagination for Product List */}
-      {/* TODO: Add pagination functionality for the list of products */}
-    </div>
+      </div>
+    </Provider>
   );
 }
 
